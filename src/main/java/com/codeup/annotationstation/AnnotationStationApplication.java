a5e1424fe7d18c98582452dc228a04dea63d2cf2
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-06-21 14:21:33 -0500

    feat: first commit

---
+++ src/main/java/com/codeup/annotationstation/AnnotationStationApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.annotationstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationStationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/annotationstation/AnnotationStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.annotationstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
