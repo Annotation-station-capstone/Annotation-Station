0e8869bb62c9d3f6f6854fdd154b3a9e02fd1c0e
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-06-21 16:54:06 -0500

    feat: working on collections controller

--- src/main/java/controllers/CollectionsController.java
+++ src/main/java/controllers/CollectionsController.java
@@ -2,10 +2,13 @@ package controllers;
 
 import daos.CollectionsRepository;
 import daos.UsersRepository;
+import models.Collection;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CollectionsController {
@@ -13,8 +16,13 @@ public class CollectionsController {
     private UsersRepository usersDao;
     private CollectionsRepository collectionsDao;
 
+    public CollectionsController(UsersRepository usersDao, CollectionsRepository collectionsDao) {
+        this.usersDao = usersDao;
+        this.collectionsDao = collectionsDao;
+    }
 
-//show all collections
+
+    //show all collections
     @GetMapping("/collections")
     public String indexPage(Model model){
         model.addAttribute("collection", collectionsDao.findAll());
@@ -29,8 +37,18 @@ public class CollectionsController {
 
     @GetMapping("collections/edit/{id}")
     public String editCollection(@PathVariable long id, Model model){
-
+        //find collection to edit
+        Collection collectionToEdit = collectionsDao.getById(id);
+        model.addAttribute("editCollection", collectionToEdit);
+        return "collection/edit";
     }
+
+    @PostMapping(value = "collections/edit/{id}")
+    public String saveEditedCollection(@PathVariable id, @ModelAttribute Collection collection){
+        //save changes
+        collection.getUser(usersDao.getById(id));
+    }
+
 
 
 }
