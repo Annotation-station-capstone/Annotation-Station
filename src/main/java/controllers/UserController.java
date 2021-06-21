5f320e7e20c6bc8da2bb4910a896f06031987b18
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-06-21 15:48:43 -0500

    feat: usersController reinstated

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -2,14 +2,37 @@ package controllers;
 
 import daos.UsersRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.codeup.annotationstation.Models.User;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+    @Controller
+    public class UserController {
+        private UsersRepository users;
+
+        //allow user to get to sign up form
+        @GetMapping("/sign-up")
+        public String signUpForm(Model model){
+            model.addAttribute("user", new com.codeup.annotationstation.Models.User());
+            return "users/sign-up";
+        }
+
+        //save user
+        @PostMapping("sign-up")
+        public String saveUser(@ModelAttribute com.codeup.annotationstation.Models.User user){
+            // still need hash strings got passwords.
+            //user.setPassword(hash) for security.
+            users.save(user);
+            return "redirect:/";
+        }
 
 
-@Controller
-public class UserController {
-    private UsersRepository users;
+
+    }
 
 
 
 
 
-}
