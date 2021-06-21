71b6737e655bee04eb0cd5f3dce2217e5e130788
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-06-21 15:18:15 -0500

    feat: added usersDao

---
+++ src/main/java/controllers/UserController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import daos.UsersRepository;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+    private UsersRepository users;
+
+
+}
