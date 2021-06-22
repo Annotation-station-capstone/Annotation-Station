c036abb7a93445589c9e885586230f9ca7c06373
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-06-22 13:59:36 -0500

    feat: section insert statement in progress

--- src/main/java/com/codeup/annotationstation/controllers/SectionController.java
+++ src/main/java/com/codeup/annotationstation/controllers/SectionController.java
@@ -1,9 +1,30 @@
 package com.codeup.annotationstation.controllers;
 
+import com.codeup.annotationstation.Models.Collection;
+import com.codeup.annotationstation.daos.CollectionsRepository;
+import com.codeup.annotationstation.daos.SectionRepository;
+import com.codeup.annotationstation.daos.UsersRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class SectionController {
     //adding fields for injected repository
-    private CollectionsRepository
+    private CollectionsRepository collectionsDao;
+    private UsersRepository usersDao;
+    private SectionRepository sectionDao;
+
+    public SectionController(CollectionsRepository collectionsDao, UsersRepository usersDao,SectionRepository sectionDao ){
+        this.collectionsDao=collectionsDao;
+        this.sectionDao=sectionDao;
+        this.usersDao= usersDao;
+    }
+
+    @GetMapping("collections/section/{id}")
+    public String insertSection(@PathVariable long id, String description, Collection collection_id){
+
+    }
+
+
 }
