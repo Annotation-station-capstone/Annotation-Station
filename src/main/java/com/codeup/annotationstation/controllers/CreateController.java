1502eae7aab037c8f0216e0c9c0b9257965d9236
Nathan Adcock <nathan.adcock43@gmail.com>
2021-06-22 20:22:00 -0500

    feat: updated the create view, js, and created a create controller

---
+++ src/main/java/com/codeup/annotationstation/controllers/CreateController.java
@@ -0,0 +1,11 @@
+package com.codeup.annotationstation.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CreateController {
+    private CreateController users;
+
+//    set to void
+
+}
