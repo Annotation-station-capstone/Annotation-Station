82c6c9f4da7bbb5f96e5604f04baa8552d2801d1
Nathan Adcock <nathan.adcock43@gmail.com>
2021-06-25 10:34:44 -0500

    feat: created NoteRepository and included notes and sections in the create controller and create service

--- src/main/java/com/codeup/annotationstation/controllers/CreateController.java
+++ src/main/java/com/codeup/annotationstation/controllers/CreateController.java
@@ -1,6 +1,8 @@
 package com.codeup.annotationstation.controllers;
 
 import com.codeup.annotationstation.Models.Collection;
+import com.codeup.annotationstation.Models.Note;
+import com.codeup.annotationstation.Models.Section;
 import com.codeup.annotationstation.service.CreateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,10 +18,24 @@ public class CreateController {
 
     @RequestMapping(value = "/collections/create", method = POST)
     @ResponseBody
-    public void add(@RequestBody Collection collection) { //In note instance you will get the json data
+    public void add(@RequestBody Collection collection) {
 
         createService.addCollection(collection);
-        //your code
+
+    }
+
+    @ResponseBody
+    public void add(@RequestBody Section section) {
+
+        createService.addSection(section);
+
+    }
+
+    @ResponseBody
+    public void add(@RequestBody Note note) {
+
+        createService.addNote(note);
+
     }
 
 
--- src/main/java/com/codeup/annotationstation/daos/SectionRepository.java
+++ src/main/java/com/codeup/annotationstation/daos/SectionRepository.java
@@ -2,6 +2,12 @@ package com.codeup.annotationstation.daos;
 
 import com.codeup.annotationstation.Models.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SectionRepository extends JpaRepository<Section, Long> {
+
+    Section findByTitle(String title);
+
+    Section findFirstByTitle(String title);
 }
