c0df09b903ecaab29535aaa93ba5f049d0755e82
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-06-30 17:17:50 -0500

    feat: index page done

--- src/main/java/com/codeup/annotationstation/SQL/Seeder.sql
+++ src/main/java/com/codeup/annotationstation/SQL/Seeder.sql
@@ -16,10 +16,130 @@ SELECT * FROM users;
 INSERT INTO collections(user_id, title, is_private, description)
 VALUES (1,'Web_dev', false, 'simple collection of notes on web development videos');
 SELECT * FROM collections;
+INSERT INTO collections(user_id, title, is_private, description)
+VALUES (2,'Home Decor', false, 'worlds greatest home decor ideas');
+SELECT * FROM collections;
+INSERT INTO collections(user_id, title, is_private, description)
+VALUES (3,'Mixology', false, 'awesome teaching for bartenders and alcohol lovers');
+SELECT * FROM collections;
+INSERT INTO collections(user_id, title, is_private, description)
+VALUES (4,'Books', false, 'A collection of my favorite artist');
+SELECT * FROM collections;
+INSERT INTO collections(user_id, title, is_private, description)
+VALUES (5,'Cooking', false, 'recipes by Chef Gordon');
+SELECT * FROM collections;
+INSERT INTO collections(user_id, title, is_private, description)
+VALUES (1,'Anime', false, 'collection of the best anime of all time');
+SELECT * FROM collections;
+INSERT INTO collections(user_id, title, is_private, description)
+VALUES (2,'Home Improvement', false, 'Tip and Tricks for home improvement');
+SELECT * FROM collections;
+INSERT INTO collections(user_id, title, is_private, description)
+VALUES (3,'Space', false, 'The Wonders of space unraveled in this collection');
+SELECT * FROM collections;
+INSERT INTO collections(user_id, title, is_private, description)
+VALUES (4,'Makeup', false, 'Makeup tutorials and tricks');
+SELECT * FROM collections;
+INSERT INTO collections(user_id, title, is_private, description)
+VALUES (5,'Tech', false, 'The world of Technology');
+SELECT * FROM collections;
 
 INSERT INTO sections (title, collection_id)
 VALUES ('HTML', 1);
 SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('CSS', 1);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Java', 1);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Lighting', 2);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Gardening', 2);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Furniture', 2);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Vodka', 3);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Cognac', 3);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Best Mixes misc,.', 3);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Fantasy', 4);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Crime', 4);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Learning', 4);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Japanese Dishes', 5);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Greek Dishes', 5);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Mexican Dishes', 5);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Demon Slayer', 6);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('My Hero', 6);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('DBZ', 6);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Tiling', 7);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Roofing', 7);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Counters', 7);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Andromeda', 8);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Sun', 8);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Moon', 8);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Sun', 8);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Foundation Tricks', 9);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Eyeshadow', 9);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Concealer', 9);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('New Tech', 10);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Coding', 10);
+SELECT * FROM sections;
+INSERT INTO sections (title, collection_id)
+VALUES ('Phones', 10);
+SELECT * FROM sections;
+
+
+
 
 INSERT INTO videos (video_url, section_id)
 VALUES ('https://www.youtube.com/watch?v=pQN-pnXPaVg', 1);
