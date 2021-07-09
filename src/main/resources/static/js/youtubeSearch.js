0adc59ab2b145de118eb80a70d01fefb05f18cc1
Darius Brown <Brown.darius.r@gmail.com>
2021-07-09 12:12:42 -0500

    youtube Search

--- src/main/resources/static/js/youtubeSearch.js
+++ src/main/resources/static/js/youtubeSearch.js
@@ -1,25 +1,32 @@
-$(document).ready(function () {
-
-    const API_KEY = "AIzaSyCBGdtk0uvKjYzDqYe87wCn9Xcw5NNWXdI";
-
-    $("search-form").submit(function (event){
-        event.preventDefault();
-
-        var searchResult = $("#searchResult").val();
-
-        videoSearch(API_KEY, searchResult, 5)
-    })
-
-    function videoSearch(API_KEY, searchResult, maxResults) {
-
-        $.get('https://www.googleapis.com/youtube/v3/search?API_KEY=' + API_KEY + '&type=video&part=snippet&maxResults=' + maxResults + '&q=' + searchResult, function (data) {
-
-                    console.log(data);
-
-                })
-    };
+//AIzaSyB3EYKIF8__Mq5SXkMKzGlC6WK8cB6228c
+$(document).ready(function() {
 
 
+    function videoSearch() {
+        let search = document.getElementById("search").value;
+        console.log(search);
+        const key = "AIzaSyCBGdtk0uvKjYzDqYe87wCn9Xcw5NNWXdI";
+        const maxResults = 5;
+
+        $.ajax({
+            url:'https://www.googleapis.com/youtube/v3/search?key=' + key +'&q=' + search,
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                var items = data.items;
+                console.log(items);
+                for(let i = 0; i < items; i++) {
+                    let video = items[i].id.videoId;
+                    console.log(video);
+                }
 
 
-    });
+                },
+            type: "GET"
+        });
+    }
+    var submitButton = document.querySelector('#submit');
+    submitButton.addEventListener('click', videoSearch);
+
+
+});
