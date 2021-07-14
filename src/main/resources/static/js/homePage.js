2db4c1f27e6798d458af58109fbc97289ea1674f
Nathan Adcock <nathan.adcock43@gmail.com>
2021-07-13 20:40:11 -0500

    feat: work on logo

--- src/main/resources/static/js/homePage.js
+++ src/main/resources/static/js/homePage.js
@@ -39,4 +39,25 @@ $(document).ready(function () {
 
     })
 
+
+    // console.clear();
+    // gsap.registerPlugin(MotionPathPlugin);
+    // gsap.set("#target", {
+    //     yPercent: -50,
+    //     xPercent: -50,
+    //     transformOrigin: "50% 50%"
+    // });
+    // gsap.set("#path2", { drawSVG: 0 });
+    //
+    // const tl = gsap.timeline({ repeat: -1 });
+    // tl.to("#target", { duration: 3, motionPath: { path: "#path1" } });
+    // tl.to("#target", { duration: 2, x: 160.52, y: 479.74 });
+    // tl.add("label");
+    // tl.to("#path2", { duration: 3, drawSVG: true }, "label");
+    // tl.to("#target", { duration: 3, motionPath: { path: "#path2" } }, "label");
+    // tl.to("#target", { duration: 2, x: 169.52, y: 107 }, "end");
+    // tl.to("#path2", { duration: 1, autoAlpha: 0 }, "end+=1");
+
+
+
 })
