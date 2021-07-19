6662c2f29193706ac817115c43f81713557f099f
Nathan Adcock <nathan.adcock43@gmail.com>
2021-07-19 11:54:57 -0500

    feat: minor change to the register  modal

--- src/mysql/mysql.sql
+++ src/mysql/mysql.sql
@@ -4,7 +4,6 @@ UPDATE collections SET title = 'Anime: Demon Slayer' WHERE id = 9;
 INSERT INTO sections (title, collection_id)
 
 values
-('how to plan for projects', 1),
-('Creating a responsive, asymmetrical design with HTML & CSS', 1),
-('Before and After pseudo elements: CSS', 1),
-('What To Expect From Your First Programming Job', 1);
+('Green Lantern Corps', 15),
+('Red Lantern Corps', 15),
+('Yellow Lantern Corps', 15);
