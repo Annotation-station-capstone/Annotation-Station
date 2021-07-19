e86407243a4a287dd6ed0742128243079de37e98
Nathan Adcock <nathan.adcock43@gmail.com>
2021-07-19 03:15:43 -0500

    feat: small Changes

---
+++ src/mysql/mysql.sql
@@ -0,0 +1,10 @@
+UPDATE collections SET title = 'Anime: Demon Slayer' WHERE id = 9;
+
+
+INSERT INTO sections (title, collection_id)
+
+values
+('how to plan for projects', 1),
+('Creating a responsive, asymmetrical design with HTML & CSS', 1),
+('Before and After pseudo elements: CSS', 1),
+('What To Expect From Your First Programming Job', 1);
