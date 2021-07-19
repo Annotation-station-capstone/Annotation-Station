6bb793bcd1faaa5b062277996ebf45d352644eed
Nathan Adcock <nathan.adcock43@gmail.com>
2021-07-19 16:17:57 -0500

    MySQL

--- src/mysql/mysql.sql
+++ src/mysql/mysql.sql
@@ -7,3 +7,30 @@ values
 ('Green Lantern Corps', 15),
 ('Red Lantern Corps', 15),
 ('Yellow Lantern Corps', 15);
+
+
+DELETE FROM collections
+WHERE id = 20;
+
+DELETE FROM sections
+WHERE id = 50;
+
+DELETE FROM notes
+WHERE id = 154;
+
+DELETE FROM notes
+WHERE id = 155;
+
+DELETE FROM notes
+WHERE id = 158;
+
+DELETE FROM notes
+WHERE id = 159;
+
+
+
+SET FOREIGN_KEY_CHECKS=0;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+select * from collections;
